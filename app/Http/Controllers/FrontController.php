99722886d5a3b5de8766a646c4a995a627fe40cc
Allen McCabe <amccabe@csatf.org>
2018-07-11 11:41:45 -0700

    Fix no connection redirect.

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -12,14 +12,25 @@ use Illuminate\Support\Facades\Config;
 class FrontController extends Controller
 {
     /**
+     * @var string $schema
+     */
+    protected $schema = 'public';
+
+    /**
      * @var array $tables
      */
     protected $tables = [];
+
+    /**
+     * @var bool $schemaChanged
+     */
+    private $schemaChanged;
 
 	/**
 	 */
 	public function index(Request $request, $database = null)
     {
+        $redirects = session('redirects', 0);
         $connectionName = $this->getConnection($database);
         $data = [
             'connections' => collect(Setting::get('connections')),
@@ -30,14 +41,22 @@ class FrontController extends Controller
             try {
                 $data['tables'] = $this->setConnection($connectionName);
             } catch (\Throwable $e) {
-                Setting::set('connection', null);
-                return redirect('/');
+                Setting::set('connection', $this->getDefaultConnection());
+                if ($redirects < 3) {
+                    session(['redirects' => ++$redirects]);
+                    return redirect('/');
+                }
             }
         } else {
             return redirect('/login');
         }
 
         return view('app', $data);
+    }
+
+    public function getDefaultConnection()
+    {
+        return env('DB_CONNECTION', 'users');
     }
 
 	/**
@@ -52,7 +71,12 @@ class FrontController extends Controller
     protected function getConnection($connectionName = null)
     {
         if (is_null($connectionName)) {
-            $connectionName = Setting::get('connection');
+            if (Setting::has('connection')) {
+                $connectionName = Setting::get('connection');
+            } else {
+                $connectionName = $this->getDefaultConnection();
+                Setting::set('connection', $connectionName);
+            }
         } else {
             Setting::set('connection', $connectionName);
         }
@@ -74,15 +98,17 @@ class FrontController extends Controller
         if ($connections) {
             foreach ($connections as $connection) {
                 if ($connection['name'] === $connectionName) {
-                    if (DB::connection()->getDatabaseName() !== $connection['database']) {
+                    if (DB::connection()->getDatabaseName() !== $connection['database'] || $this->schemaChanged) {
                         DB::purge('dynamic');
                         Config::set('database.connections.dynamic.host', $connection['host']);
                         Config::set('database.connections.dynamic.database', $connection['database']);
                         Config::set('database.connections.dynamic.username', $connection['username']);
                         Config::set('database.connections.dynamic.password', $connection['password']);
+                        Config::set('database.connections.dynamic.schema', $this->schema);
                         Config::set('database.default', 'dynamic');
                         DB::reconnect('dynamic');
                         Schema::connection('dynamic')->getConnection()->reconnect();
+                        $this->schemaChanged = false;
                     }
                     $tables = $this->getTables($connection['database']);
                 }
@@ -91,9 +117,30 @@ class FrontController extends Controller
         return $tables;
     }
 
+    /**
+     * @param $schema
+     * @return $this
+     */
+    protected function setSchema($schema, $reconnect = true)
+    {
+        if ($schema && $schema !== $this->schema) {
+            $this->schema = $schema;
+            if ($reconnect) {
+                DB::purge('dynamic');
+                Config::set('database.connections.dynamic.schema', $this->schema);
+                Config::set('database.default', 'dynamic');
+                DB::reconnect('dynamic');
+                Schema::connection('dynamic')->getConnection()->reconnect();
+            } else {
+                $this->schemaChanged = true;
+            }
+        }
+        return $this;
+    }
+
     protected function getTables($database = null, $bustcache = false)
     {
-        $sql = 'SELECT * FROM pg_catalog.pg_tables WHERE schemaname = \'public\' ORDER BY tablename';
+        $sql = 'SELECT * FROM pg_catalog.pg_tables WHERE schemaname = \''.$this->schema.'\' ORDER BY tablename';
         if (is_null($database)) {
             $database = DB::connection()->getDatabaseName();
         }
